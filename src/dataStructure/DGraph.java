d8fb5424827c9bc6fb3a5c2dd6fc7aadf0ec50f5
Eliel <eliel7114@gmail.com>
2019-12-21 23:08:14 +0200

    update DGraph class and implement functions

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -1,73 +1,142 @@
 package dataStructure;
 
 import java.util.Collection;
+import java.util.LinkedHashMap;
 
-public class DGraph implements graph{
+public class DGraph implements graph {
+
+	private LinkedHashMap<Integer, node_data> vertices;
+	private LinkedHashMap<Integer, LinkedHashMap<Integer, edge_data>> edges;
+	private int mc;
+	private int edgesSize;
+
+	public DGraph() {
+		this.vertices = new LinkedHashMap<Integer, node_data>();
+		this.edges = new LinkedHashMap<Integer, LinkedHashMap<Integer, edge_data>>();
+	}
+
+	@SuppressWarnings("unchecked")
+	public DGraph(DGraph g) {
+		this.vertices = (LinkedHashMap<Integer, node_data>) g.vertices.clone();
+		this.edges = (LinkedHashMap<Integer, LinkedHashMap<Integer, edge_data>>) g.edges.clone();
+		this.mc = g.getMC();
+		this.edgesSize = g.edgeSize();
+		
+	}
+
+	public DGraph(int vertices) {
+		this();
+		for (int i = 0; i < vertices; i++) {
+			node_data n = new Node();
+			this.vertices.put(n.getKey(), n);
+		}
+	}
 
 	@Override
 	public node_data getNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.vertices.get(key);
 	}
 
 	@Override
 	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.edges.get(src).get(dest);
 	}
 
 	@Override
 	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
-		
+		this.vertices.put(n.getKey(), n);
+		this.mc++;
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-		
+		node_data s = this.vertices.get(src);
+		node_data d = this.vertices.get(dest);
+		if(s == null || d == null) {
+			System.out.println("Cannot connect between this vertices , some of them or maybe both , are null");
+			return;
+		}
+		LinkedHashMap<Integer, edge_data> hp = this.edges.get(src);
+		if(hp == null) {
+			// new src , create new linked list of edge values.
+			hp = new LinkedHashMap<Integer, edge_data>();
+		}
+		edge_data e = new Edge(src,dest,w);
+		hp.put(dest, e);
+		this.edges.put(src, hp);
+		this.edgesSize++;
+		this.mc++;
 	}
 
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.vertices.values();
 	}
 
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedHashMap<Integer, edge_data> edgesSrc = this.edges.get(node_id);
+		if (edgesSrc == null)
+			return null;
+		return edgesSrc.values();
 	}
 
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedHashMap<Integer, edge_data> e = this.edges.remove(key);
+		node_data n = this.vertices.remove(key);
+		
+		if(e.values() != null && n != null) this.mc++;
+		
+		if(e.values() != null) this.edgesSize -= e.size();
+		
+		return n;
+		
 	}
 
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		edge_data e = this.edges.get(src).remove(dest);
+		if(e != null) this.edgesSize--;
+		
+		return e;
 	}
 
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.vertices.size();
 	}
 
 	@Override
 	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.edgesSize;
 	}
 
 	@Override
 	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.mc;
+	}
+	
+	public String verticesString() {
+		String s = "";
+		for (int key : this.vertices.keySet()) {
+			s += this.vertices.get(key) + "\n";
+		}
+		return s;		
+	}
+	
+	public String toString() {
+		String s = "";
+		for (int src : this.edges.keySet()) {
+			LinkedHashMap<Integer, edge_data> edgeSrc = this.edges.get(src);
+			if(edgeSrc != null) {
+				for (int dest : edgeSrc.keySet()) {
+					s += "HashMap data - src: " + src + " dest: " + dest + "\n" 
+							 + edgeSrc.get(dest) + "\n\n";
+				}
+			}
+		}
+		return s;
 	}
 
 }
