1775265212df2263bfda5c37ae559b57006c669c
Eliel <eliel7114@gmail.com>
2019-12-25 22:54:28 +0200

    Node documentation

--- src/dataStructure/Node.java
+++ src/dataStructure/Node.java
@@ -12,11 +12,18 @@ public class Node implements node_data {
 	private int tag;
 	static int uuid = 1;
 
+	/**
+	 * Empty constructor
+	 */
 	public Node() {
 		setKey();
 		this.initNode();
 	}
 	
+	/**
+	 * Deep copy constructor
+	 * @param n - the node to copy from
+	 */
 	public Node(node_data n) {
 		this.key = n.getKey();
 		this.weight = n.getWeight();
@@ -25,72 +32,117 @@ public class Node implements node_data {
 		this.tag = n.getTag();
 	}
 
+	/**
+	 * Constructor with location
+	 * @param location - Point3D location
+	 */
 	public Node(Point3D location) {
 		setKey();
+		this.initNode();
 		this.location = new Point3D(location);
 	}
 
+	/**
+	 * Getter for key field
+	 */
 	@Override
 	public int getKey() {
 		return this.key;
 	}
 
+	/**
+	 * Setter for key , using static uuid field
+	 */
 	private void setKey() {
 		this.key = uuid;
 		uuid++;
 	}
 
+	/**
+	 * Getter for location
+	 */
 	@Override
 	public Point3D getLocation() {
 		return new Point3D(this.location);
 	}
 
+	/**
+	 * Setter for location
+	 */
 	@Override
 	public void setLocation(Point3D p) {
 		this.location = new Point3D(p);
 	}
 
+	/**
+	 * Getter for weight
+	 */
 	@Override
 	public double getWeight() {
 		return weight;
 	}
 
+	/**
+	 * Setter for weight
+	 */
 	@Override
 	public void setWeight(double w) {
 		this.weight = w;
 	}
 
+	/**
+	 * Getter for info
+	 */
 	@Override
 	public String getInfo() {
 		return this.info;
 	}
 
+	/**
+	 * Setter for info
+	 */
 	@Override
 	public void setInfo(String s) {
 		this.info = s;
 	}
 
+	/**
+	 * Getter for tag
+	 */
 	@Override
 	public int getTag() {
 		return this.tag;
 	}
 
+	/**
+	 * Setter for tag
+	 */
 	@Override
 	public void setTag(int t) {
 		this.tag = t;
 	}
 
+	/**
+	 * Override toString
+	 */
 	public String toString() {
 		return "Node data - " + " key: " + this.key + " location: " + this.location + " weight: " + this.weight + " info: "
 				+ this.info + " tag: " + this.tag;
 	}
 	
+	/**
+	 * Gets random x and y and return new location with those positions
+	 * @return
+	 */
 	private static Point3D getRandomLocation() {
 		int randomX = new Random().nextInt(400);
 		int randomY = new Random().nextInt(400);
 		return new Point3D(randomX,randomY);
 	}
 	
+	/**
+	 * Function to reset node for the algorithms
+	 */
 	public void initNode() {
 		setTag(0);
 		setInfo("");
