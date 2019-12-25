7e339d0bc703255bf25e0b66356a2356fb02c035
Eliel <eliel7114@gmail.com>
2019-12-25 22:56:14 +0200

    Edge documentation

--- src/dataStructure/Edge.java
+++ src/dataStructure/Edge.java
@@ -7,12 +7,22 @@ public class Edge implements edge_data {
 	private String info;
 	private int tag;
 	
+	/**
+	 * Constructor with params
+	 * @param src
+	 * @param dest
+	 * @param weight
+	 */
 	public Edge(int src,int dest,double weight) {
 		this.src = src;
 		this.dest = dest;
 		this.weight = weight;
 	}
 	
+	/**
+	 * Deep copy constructor
+	 * @param e
+	 */
 	public Edge(edge_data e) {
 		this.src = e.getSrc();
 		this.dest = e.getDest();
@@ -21,41 +31,65 @@ public class Edge implements edge_data {
 		this.tag = e.getTag();
 	}
 
+	/**
+	 * Getter for src
+	 */
 	@Override
 	public int getSrc() {
 		return this.src;
 	}
 
+	/**
+	 * Getter for dest
+	 */
 	@Override
 	public int getDest() {
 		return this.dest;
 	}
 
+	/**
+	 * Getter for weight
+	 */
 	@Override
 	public double getWeight() {
 		return this.weight;
 	}
 
+	/**
+	 * Getter for info
+	 */
 	@Override
 	public String getInfo() {
 		return this.info;
 	}
 
+	/**
+	 * Setter for info
+	 */
 	@Override
 	public void setInfo(String s) {
 		this.info = s;
 	}
 
+	/**
+	 * Getter for tag
+	 */
 	@Override
 	public int getTag() {
 		return this.tag;
 	}
 
+	/**
+	 * Setter for tag
+	 */
 	@Override
 	public void setTag(int t) {
 		this.tag = t;
 	}
 	
+	/**
+	 * Override toString
+	 */
 	public String toString() {
 		return "Edge data - " + " src: " + this.src + " dest: " + this.dest + " weight: " + this.weight + " info: "
 				+ this.info + " tag: " + this.tag + " ";
